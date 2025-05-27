b8ef8de6338db16f0c9f5778b425c8582653a5c4
codeAyush05 <ayushh.dev05@gmail.com>
2025-05-27 19:29:55 +0530

    pratice.js

---
+++ src/practice.js
@@ -0,0 +1,13 @@
+// you have an array of students marks
+
+let marks = [25, 30, 35, 40, 55]
+
+// 1. to give each students 5 as a bonus mark
+
+const addFive = marks
+  .map((mark) => mark + 5)
+  .filter((mark) => mark >= 40)
+  .reduce((acc, mark) => {
+    return acc + mark
+  }, 0)
+console.log(addFive)
